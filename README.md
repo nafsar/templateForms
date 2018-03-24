f70d42048fc06b181e29de679d946278d532f095
nafsar <n.afsar@gmail.com>
2018-03-23 19:14:45 -0600

    template form

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.2.
 
+## Demo
+
+(https://nafsar.github.io/demo/templateform/)
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  emails = ['', 'naser@angular.io', 'templateFormat@angular.io', 'test@gm.com',
+    '', 'form@uiux.bi'];
+
+  model = new Data('Earth', this.emails[0], 'Angular Template Driven Forms');
+
+  submitted = false;
+  title = 'name';
+  emailAddress = 'Email Address';
+  text = 'Message';
+  emailAddressRes = ''; titleRes = ''; textRes = '';
+  onSubmit(result) {
+    this.titleRes = result.name;
+    this.emailAddressRes = result.myEmails;
+    this.textRes = result.textMessage;
+    this.submitted = true;
+  }
+
+
+}
+
+export class Data {
+  constructor(
+    public name: string,
+    public myEmails?: string,
+    public textMessage?: string
+  ) { }
+
 }
